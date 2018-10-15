521bc47dd50a7034a2648e44ffab3839c38c9c68
Petr <petr_lutik@mail.ru>
2018-10-16 01:01:25 +0400

    Решена задачка 1496

--- timus1496.cpp
+++ timus1496.cpp
@@ -1,7 +1,28 @@
 ﻿#include <iostream>
+#include <map>
+#include <string>
 
 int main() {
+  // Read length of names list.
+  int names_len = 0;
+  std::cin >> names_len;
 
+  std::map<std::string, int> names;
 
+  std::string name;
+  // Dummy read.
+  std::getline(std::cin, name);
+
+  // Read names.
+  for(int i = 0; i < names_len; ++i) {
+    std::getline(std::cin, name);
+    names[name]++;
+  }
+
+  // Find and print all names involved to the list more than one time.
+  for(auto it: names) {
+    if(it.second > 1)
+      std::cout << it.first << std::endl;
+  }
   return 0;
 }
